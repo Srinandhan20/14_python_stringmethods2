adaada865898b2cd8c3c24330697452a0b8e6aa6
Srinandhan20 <107259556+Srinandhan20@users.noreply.github.com>
2022-07-10 14:46:23 +0530

    Add files via upload

---
+++ stringmethod2.py
@@ -0,0 +1,26 @@
+# w='Hey Bye'
+# print(w.find('H'))
+# print(w.find('y'))
+# print(w.find('o'))
+# d='mobileboot'
+# print(d.count('b'))
+# print(d.count('o'))
+# print(d.count('u'))
+# q='heycode'
+# print(q.isalpha())
+# z='hey code'
+# print(q.isalpha())
+
+
+s= 'hi12'
+print(s.isalnum())
+r='hi 12'
+print(r.isalnum())
+x='promax 34'
+print(x.isdigit())
+y='4878_6'
+print(y.isdigit())
+p='456'
+print(p.isdigit())
+w='Hello'
+print(w.join('*'))
